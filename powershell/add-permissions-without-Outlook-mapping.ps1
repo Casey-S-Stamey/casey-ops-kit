18788462c98ccbe2b389312a9a3465fbb2d4387b
Casey-S-Stamey <casey.s.stamey@gmail.com>
2025-09-19 08:37:19 -0400

    feat(powershell): add-permissions-without-Outlook-mapping.ps1

---
+++ powershell/add-permissions-without-Outlook-mapping.ps1
@@ -0,0 +1,13 @@
+﻿<# 
+Name: add-permissions-without-Outlook.ps1
+Purpose: Give someone access to a mailbox without automapping to Outlook.This can be used to monitor on the webportal and not have it take up space on desktop Outlook. 
+When to use: someone needs web access to a mailbox but does not want it in Outlook
+Usage:
+  .\add-permissions-without-Outlook.ps1 -OutputPath -N/A
+
+Notes: Requires Exchange Online module and proper RBAC.
+#>
+
+
+
+Add-MailboxPermission usermailbox@domain.com -User usertogetpermissions@domain.com -AccessRights FullAccess -InheritanceType All -AutoMapping $False
